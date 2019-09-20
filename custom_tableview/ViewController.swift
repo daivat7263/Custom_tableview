585b4b0761feba8d2d26d83102ac4739f73ed99b
COE-02 <coe-02@coe-02s-imac.local>
2019-09-20 14:32:04 +0530

    Custom_table view

--- custom_tableview/ViewController.swift
+++ custom_tableview/ViewController.swift
@@ -8,7 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource
+{
+    let Studentdetails = [
+    
+        ["Name" : "Pooja" , "Grade" : "A"],
+        ["Name" : "Mahesh" , "Grade" : "A"],
+         ["Name" : "Suresh" , "Grade" : "B"],
+          ["Name" : "Krupesh" , "Grade" : "C"],
+           ["Name" : "Dinesh" , "Grade" : "A"],
+            ["Name" : "Kamlesh" , "Grade" : "D"],
+         ["Name" : "Ramesh" , "Grade" : "C"]
+    ]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return Studentdetails.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let details = Studentdetails[indexPath.row]
+    
+    cell.textLabel?.text = details ["Name"]
+    cell.detailTextLabel?.text = details ["Grade"]
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
